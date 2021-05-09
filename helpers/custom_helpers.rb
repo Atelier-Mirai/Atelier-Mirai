80e74134d492177cc9b96aff1afdacf42fb3ef74
Atelier-Mirai <contact@atelier-mirai.net>
2021-05-09 23:45:05 +0900

    math.pdf

--- helpers/custom_helpers.rb
+++ helpers/custom_helpers.rb
@@ -1,4 +1,21 @@
-include HtmlBuilder
+# include HtmlBuilder
+
+module HtmlBuilder
+  def markup(tag_name = nil, options = {})
+    root = Nokogiri::HTML::DocumentFragment.parse("")
+    Nokogiri::HTML::Builder.with(root) do |doc|
+      if tag_name
+        doc.method_missing(tag_name, options) do
+          yield(doc)
+        end
+      else
+        yield(doc)
+      end
+    end
+    root.to_html.html_safe
+  end
+end
+
 
 def some_method
   # ...何らかの処理を追加...
