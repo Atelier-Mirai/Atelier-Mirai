2b1c28ff415501b20ed04338ebf8d52790faa56d
AtelierMirai <mirai@AtelierMirainoMac-mini.local>
2022-06-07 19:24:46 +0900

    MacBook Air 価格改定ほか

---
+++ build/javascripts/site.js
@@ -0,0 +1,37 @@
+$(function(){
+  // fix main menu to page on passing
+  // $('.main.menu').visibility({
+  //   type: 'fixed'
+  // });
+  // $('.overlay').visibility({
+  //   type: 'fixed',
+  //   offset: 80
+  // });
+  // // lazy load images
+  // $('.image').visibility({
+  //   type: 'image',
+  //   transition: 'vertical flip in',
+  //   duration: 500
+  // });
+  // // show dropdown on hover
+  // $('.main.menu  .ui.dropdown').dropdown({
+  //   // on: 'hover'
+  // });
+  // show dropdown on hover
+  $('.ui.dropdown').dropdown({
+    // on: 'click'
+    // on: 'hover'
+  });
+  //
+  // $('.masthead').visibility({
+  //   once: false,
+  //   onBottomPassed: function() {
+  //     $('.fixed.menu').transition('fade in');
+  //   },
+  //   onBottomPassedReverse: function() {
+  //     $('.fixed.menu').transition('fade out');
+  //   }
+  // });
+  // $('.ui.sidebar').sidebar('attach events', '.toc.item');
+  //
+});
